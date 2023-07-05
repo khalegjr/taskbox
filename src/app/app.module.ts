a8d6ee52846e921d34b13805eed130811cd12c03
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-05 12:22:50 -0300

    implements the wrapper for the Pure Task List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task/task.component';
+import { PureTaskListComponent } from './components/pure-task-list/pure-task-list.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    TaskListComponent
+    TaskListComponent,
+    PureTaskListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
